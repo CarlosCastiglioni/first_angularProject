a3d2e1f699d7d7b8ea87e6fcb5ad0dc8f4df0f26
Carlos Alberto Castiglioni Junior <juniorcastiglioni@gmail.com>
2021-03-03 09:11:53 -0300

    adding the final array and new functions

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { Course } from './course'
+import { CourseService } from './course.service'
 
 @Component({
   selector: 'app-course-list',
@@ -10,29 +11,10 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = []
 
+  constructor(private courseService: CourseService) { }
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular: Forms',
-        imageUrl: '/assets/img/forms.png',
-        price: 99.99,
-        code: 'XPS-8796',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: 'December, 2, 2019',
-      },
-      {
-        id: 2,
-        name: 'Angular: HTTP',
-        imageUrl: '/assets/img/http.png',
-        price: 45.99,
-        code: 'LKL-1094',
-        duration: 80,
-        rating: 4,
-        releaseDate: 'November, 4, 2019'
-      }
-    ]
+    this.courses = this.courseService.retrieveAll()
   }
 
 }
